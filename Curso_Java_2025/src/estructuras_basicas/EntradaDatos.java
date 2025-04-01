81437874215f7a134ef98ce38ea6dedfadc1446f
m-blazquez <m.blazquezva@gmail.com>
2025-04-01 22:29:33 +0200

    Vídeo 'Entrada y salida de datos II. Clase JOptionPane'

--- Curso_Java_2025/src/estructuras_basicas/EntradaDatos.java
+++ Curso_Java_2025/src/estructuras_basicas/EntradaDatos.java
@@ -2,12 +2,14 @@ package estructuras_basicas;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class EntradaDatos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Leer datos con la clase Scanner
+		// LEER DATOS CON LA CLASE Scanner
 		
 		Scanner entrada = new Scanner(System.in); // Crear una nueva instancia de la clase Scanner con el constructor que coge datos con System.in
 		
@@ -25,6 +27,17 @@ public class EntradaDatos {
 		
 		// Cuando hayas terminado con el Scanner, cerralo para no desperdiciar recursos
 		entrada.close();
+		
+		
+		// LEER DATOS CON LA CLASE JOptionPane
+		
+		String nombre_usuario2 = JOptionPane.showInputDialog("Introduce tu nombre, por favor");
+		
+		int edad2 = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad, por favor"));
+		
+		double salario = Double.parseDouble(JOptionPane.showInputDialog("Introduce tu salario, por favor"));
+				
+		System.out.println("Te llamas "+ nombre_usuario2 + ", tienes " + edad2 + " años y tu salario es de " + salario + "€");
 	}
 
 }
