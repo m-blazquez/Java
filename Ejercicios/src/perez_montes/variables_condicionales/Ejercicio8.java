cee96d22dd964304b846c5d74c5fdbcd4c63b102
m-blazquez <m.blazquezva@gmail.com>
2025-04-06 22:30:19 +0200

    Ejercicio 8 libro F.M. Pérez Montes

---
+++ Ejercicios/src/perez_montes/variables_condicionales/Ejercicio8.java
@@ -0,0 +1,44 @@
+package perez_montes.variables_condicionales;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ejercicio8 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * ENUNCIADO: Pedir dos números y decir cual es el mayor o si son iguales.
+		 */
+
+		// Declarar e inicializar variables, seran doubles porque no se especifica que tengan que ser enteros
+		double numero1 = 0.0;
+		double numero2 = 0.0;
+		double mayor = 0.0;
+
+		// Crear un objeto de la clase scanner para ingresar el radio
+		Scanner entrada = new Scanner(System.in);
+		entrada.useLocale(Locale.US); // Esto es para que coja los decimales con punto en vez de con comas
+
+		// Solicitar al usuario que introduzca los dos números:
+		System.out.println("Introduce el valor del primer número:");
+		numero1 = entrada.nextDouble();
+
+		System.out.println("Introduce el valor del segundo número:");
+		numero2 = entrada.nextDouble();
+
+		// Determinar si son iguales y, si no lo son, averiguar cuál es el mayor y devolver el resultado
+		if (numero1 == numero2) {
+			
+			System.out.println("Los dos números son iguales");
+			
+		} else {
+			
+			mayor = Math.max(numero1, numero2);
+			System.out.println("Entre " + numero1 + " y "+ numero2 + ", el mayor número es " + mayor);
+			
+		}
+
+	}
+}
